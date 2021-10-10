3cf9256306c9632bafa8c573fe74634f5f0fbdfb
Kristiqan Stefanov <kristiqanstefanov@gmail.com>
2021-10-10 21:16:20 +0300

    Some Simple Console Programs

---
+++ Homework for 24.01.2021/Source.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    //Task 1
+
+	/*for (double n = 1; n <= 100; n++) {
+
+		cout << n / (n + 1) << " ";*/
+
+	/*}*/
+
+
+	//Task 2  Write a program that prints the multiplication table in the following format :
+
+	///*for (int i = 1; i <= 10; i++) {
+	//	
+	//	for (int j =1; j <= 10; j++) {
+
+	//		
+	//		cout << j << " * " << i;
+	//		
+	//		cout << "   ";
+	//	}  
+
+	//		
+	//	cout << endl;
+	//}*/
+
+	
+	//Task 3 Write a program that enters the number n and prints a triangle of asterisks as follows :
+	
+	/*int n;
+	cout << "n= ";
+	cin >> n;
+
+
+	for (int i = 1; i <= n; i++) {
+
+		for (int k = n - i; k > 0; k--)
+			cout << " ";
+
+		for (int j = 1; j <= i; j++) {
+
+			cout << "*";
+		}
+
+		cout << endl;
+   }*/
+
+	   
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+
+
+
+
+}
+
