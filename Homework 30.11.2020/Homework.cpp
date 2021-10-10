3c2f0c62c8693823db1dba43d8112da0d0b360b2
Kristiqan Stefanov <kristiqanstefanov@gmail.com>
2021-10-11 00:22:33 +0300

    Some Simple Console Programs

---
+++ Homework 30.11.2020/Homework.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	
+	//// Write a program that reads 2 numbers (the sides of a rectangle) (int a and b). The program solves and prints the face of a rectangle :
+	//
+	//cout << "exercise 1 result:" << endl;
+	//
+	//int a = 7;
+	//int b = 20;
+	//
+	//cout << endl;
+	//
+	//cout << "a x b=" << a * b << endl;
+
+
+	
+    //Enter two values ​​from the console n and m. Print the text "n is greater than m" if n > = m and the text "m is greater than n" otherwise.
+	
+	/*cout << endl;
+	
+	cout << "exercise 2 result:\n";
+	
+	cout << endl;
+	
+	cout << "Enter numbers:\n";
+    
+	int n;
+    int m;
+
+	cout << "n: ";
+	cin >> n;
+
+	cout << "m: ";
+	cin >> m;
+
+	if (n >= m) {
+		cout << "n is greater then m \n";
+	}
+	
+	else{ cout << "m is greater then n \n";
+	}*/
+		
+	//Enter two Boolean values ​​from the console.On the entered values apply the logical operators &&,||,^and print the result on the screen.
+	 
+	/*bool a;
+	bool b;
+	cout << "Enter Values";
+	cin >> a >> b;*/
+	
+	/*if (a == 0 &&  b == 1) {
+		cout << "&&\n";
+	}*/
+
+	/*if (a == 0 || b == 1) {
+		cout << "||\n";
+
+	}*/
+
+	/*if (a == 0 ^ b == 1) {
+		cout << "^\n";
+	}*/
+	
+	/*double speed;
+	cout << "Enter speed value\n";
+	cin >> speed;
+
+	if (speed <= 10) {
+		
+		cout << "slow\n";
+	}
+	else if (speed > 10 && speed <= 50) {
+		cout << "average\n";
+	}
+	else if (speed > 50 && speed <= 150) {
+		cout << "fast\n";
+
+	}
+	else if (speed > 150 && speed <= 1000) {
+		cout << "ultra fast\n";
+	}
+	else if (speed > 1000) {
+		cout << "extremely fast\n";
+	}*/
+		
+	/*int grade;
+	cout << "enter grade:\n";
+	cin >> grade;
+
+	switch (grade) {
+	
+	case 2: cout << "Valid grade\n"; break;
+	case 3: cout << "Valid grade\n"; break;
+	case 4: cout << "Valid grade\n"; break; 
+	case 5: cout << "Valid grade\n"; break;
+	case 6: cout << "Valid grade\n"; break;
+	default:cout << "Invalid grade\n";*/
+	
+	}
+		
+
+	return 0;
+}
