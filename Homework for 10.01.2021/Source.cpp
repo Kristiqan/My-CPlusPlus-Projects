c3c595c39d07852a13a9129fe0eaa47737bd1b48
Kristiqan Stefanov <kristiqanstefanov@gmail.com>
2021-10-10 23:37:43 +0300

    Some Simple Console Programs

---
+++ Homework for 10.01.2021/Source.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//Task 1 :Derive the following sequence using the while loop 7 14 21 28 35 42 49 56 63 70 77 84 91 98
+	
+	/*cout << "Task1:\n";
+	
+	int num = 7;
+
+    while (num <= 98) {
+		cout << num << " ";
+		num = num + 7;
+
+	}*/
+
+	// Task 2 The sequence of task 1 should be displayed without even numbers!
+
+	/*cout << "Task2:\n";
+
+	 int num = 7;
+
+    while (num <= 98) {
+		
+	if (num % 2 != 0) {
+
+		cout << num << " ";
+	     }
+		
+	     num = num + 7 ;
+	
+	}*/
+	
+
+    // Task 3 Derive the following sequence using the while loop 1 1 9 27 81 243 729 2187
+
+	//cout << "Task 3:\n";
+
+	//int num = 1;
+
+	//while (num <= 2187) {
+
+		//cout << num << " ";
+
+		//num = num * 3;
+	//}
+			
+	//Task 4 The program must read a number from standard input. Display all numbers from 1 to n(the number from the user) that are divided simultaneously into 3 and 5.
+
+	//cout << "Task 4:\n";
+
+	//int n;
+	
+	//cin >> n;
+
+	//for (int n = 1; n<= 40 ; n++ ) {
+		
+		//if (n % 3 == 0 && n % 5 == 0) {
+			
+			//cout << n << " ";
+
+		//}
+		
+
+	//}
+		
+
+	//Task 5 To compile a program that prints when entering 1 integer the name of the month that corresponds to the number.If the number is invalid to give Error message.
+
+	//cout << "Task5:\n";
+
+	//int month;
+	//cout << "month: ";
+	//cin >> month;
+
+	//switch (month) {
+	//case 1:cout << "january\n"; break;
+	//case 2: cout << "February\n"; break;
+	//case 3:cout << "March\n"; break;
+	//case 4:cout << "April\n"; break;
+	//case 5:cout << "May\n"; break;
+	//case 6:cout << "June\n"; break;
+	//case 7:cout << "July\n"; break;
+	//case 8:cout << "August\n"; break;
+	//case 9:cout << "September\n"; break;
+	//case 10:cout << "October\n"; break;
+	//case 11:cout << "November\n"; break;
+	//case 12:cout << "December\n"; break;
+	//default: cout << "invalid month\n"; break;
+	//}
+
+
+	// Task 6 A year is introduced. We want to see if she's happy, and she's happy when there is at least one return digit
+
+	/*cout << "Task6:\n";
+
+	int a, b, c, d, n;
+	
+	cout << "input Value:";
+	cin >> n;
+	
+	a = n / 1000;
+	b = (n / 100) % 10;
+	c = (n % 100) / 10;
+	d = n % 10;
+
+	if (a == b ||a== c || a== d || b== c || b== d ||c==d) {
+
+		cout << "Happy year";
+
+	}
+	else  {
+		
+		cout << "Not a Happy year";
+	}*/
+
+    
+      return 0;
+
+
+}
