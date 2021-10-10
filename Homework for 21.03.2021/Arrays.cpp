d536ef0fcf5793d081e88a93172b67969c1ebfa2
Kristiqan Stefanov <kristiqanstefanov@gmail.com>
2021-10-11 00:05:47 +0300

    Some Arrays

---
+++ Homework for 21.03.2021/Arrays.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+
+//Write a program that reads 7 items from the console and then writes them in an array. The program has to display all numbers multiples of 3, which are between 4 and 99.
+
+//   cout <<"Task 1: \n";
+
+//     int* arr1 = new int[7];
+     
+// 	for (int i = 0; i < 7; i++) {
+// 		cin >> arr1[i];
+// 	}
+//    cout <<"--------------\n";
+// 	for (int i = 0; i < 7; i++) {
+		
+// 		if((arr1[i] % 3 == 0 )&&((arr1[i]>4)&&(arr1[i]<99))){
+		
+// 		 cout<<arr1[i]<<endl;
+		
+// 		}
+		
+			
+// 	}
+	
+// 	delete[] arr1;
+   
+   //----------------------------------------------------------------------------------------------------
+
+   //Write a program that reads 7 items from the console and then writes them in an array. The program has to display on the console only even numbers, if any.
+//     cout<<"Task 2: \n";
+
+//     int* arr1 = new int[7];
+     
+// 	for (int i = 0; i < 7; i++) {
+// 		cin >> arr1[i];
+// 	}
+//    cout <<"--------------\n";
+// 	for (int i = 0; i < 7; i++) {
+		
+// 		if(arr1[i] % 2 == 0 ){
+		
+// 		 cout<<arr1[i]<<endl;
+		
+// 		}
+		
+			
+// 	}
+	
+// 	delete[] arr1;
+    
+
+   
+
+ return 0;
+}
+
