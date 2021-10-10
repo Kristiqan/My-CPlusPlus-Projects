8d0ed5fe276cf1311a7bdd20e2830074edc40630
Kristiqan Stefanov <kristiqanstefanov@gmail.com>
2021-10-10 23:46:16 +0300

    Some Simple Console Programs

---
+++ Homework for 07.03.2021/Source.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+//const int SIZE = 10;
+
+//const int SIZE1 = 7;
+
+//const int SIZE2 = 7;
+
+//const int N = 8;
+
+int main() {
+
+	//Task1 Write a program that reads the elements in one dimension  array of length 10 from the console.Then print the elements of the array.
+
+	/*cout << "Task 1:\n";
+
+	int arr[SIZE];
+
+	for (int i = 0; i < SIZE; i++) {
+
+		cin >> arr[i];
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < SIZE; i++) {
+
+		cout << arr[i] << " ";
+	}*/
+
+	//Task2 Write a program that reads the elements in one dimensionarray with a length of 7 from the console. Print the sum of all elements of the array.
+
+	/*cout << "Task 2:\n";
+
+	int arr1[SIZE1];
+
+	for (int i = 0; i < SIZE1; i++) {
+
+		cin >> arr1[i];
+	}
+
+	int sum = 0;
+
+	for (int i = 0; i < SIZE1; i++) {
+		sum += arr1[i];
+
+	
+	}
+	cout << "sum = " << sum << endl;*/
+
+	//Task 3 Write a program that reads the elements in one dimension array with a length of 7 from the console.The program must do all even numbers from the array to 0 and bring the array to the screen.
+
+	/*cout << "Tast 3:\n";
+
+	int arr2[SIZE2];
+	
+	cout << "Enter only " << SIZE2 << " Numbers: ";
+
+	for (int i = 0; i < SIZE2; i++) {
+		
+		cin >> arr2[i];
+
+		if (arr2[i] % 2 == 0) {
+			
+			arr2[i] = 0;
+		}
+
+		cout << arr2[i] << " ";
+
+	}*/
+
+	//Task 4 Write a program that reads the elements in one dimension array of length N(N is 8) from the consoleand output the average value of the entered numbers.
+
+	/*cout << "Task 4:\n";
+
+	int arr3[N];
+
+	for (int i = 0; i <N; i++) {
+
+		cin >> arr3[i];
+	}
+
+	double  averagevalue = 0;
+
+	for (int i = 0; i < N; i++) {
+		
+		averagevalue += arr3[i];
+
+	}
+	
+	cout << " averagevalue = " << averagevalue / 8 << endl;*/
+
+		
+	return 0;
+}
+
+	
+	
